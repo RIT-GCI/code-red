e0e3ffa420e3b53aabed83ad1f5fafa36e0c9bc8
gido5731 <gido@gido.click>
2023-10-08 00:39:39 -0400

    Actually working demo of terraform

--- main.tf
+++ main.tf
@@ -11,7 +11,28 @@ required_version = ">= 0.14.0"
 
 # Configure the OpenStack Provider
 provider "openstack" {
-  user_name   = var.openstack_username
-  password    = var.openstack_password
+  application_credential_secret    = var.openstack_application_credential_secret
+  application_credential_name = var.openstack_application_credential_name
+  user_name      = var.openstack_username
   auth_url    = var.openstack_auth_url
+  tenant_id     = var.openstack_project_id
+  user_domain_name = var.openstack_user_domain_name
+  region = var.openstack_region_name
+  project_domain_id = var.openstack_project_domain_id
+  allow_reauth = false
+  
+}
+
+
+resource "openstack_compute_instance_v2" "testubu" {
+  name              = "testubu"
+  image_name        = "UbuntuJammy2204"
+  flavor_name       = "small"
+  key_pair          = "gframe"
+  security_groups   = ["default"]
+
+
+  network {
+    name = "MAIN-NAT"
+  }
 }
--- variables.tf
+++ variables.tf
@@ -1,17 +1,52 @@
-variable "openstack_username" {
-    description = "OpenStack username"
+variable "openstack_application_credential_secret" {
+    description = "OpenStack token"
     type = string
 }
 
-#Can't find a sane way to do auth via tokens...
-variable "openstack_password" {
-    description = "OpenStack password"
+variable "openstack_application_credential_name" {
+    description = "OpenStack application credential name"
+    type = string
+}
+
+variable "openstack_username" {
+    description = "OpenStack application credential name"
     type = string
 }
 
 variable "openstack_auth_url" {
     description = "OpenStack auth url"
     type = string
-    default = "https://openstack.cyberrange.rit.edu:5000"
+    # for some reason if you don't add /v3 it uses port 443...
+    # this took at least an hour to figure out
+    default = "https://openstack.cyberrange.rit.edu:5000/v3"
 }
 
+# variable "openstack_project_name" {
+#     description = "OpenStack project ID"
+#     type = string
+#     default = "code-red"
+# }
+
+variable "openstack_project_id" {
+    description = "OpenStack project ID"
+    type = string
+    default = "b086544404524220a92e43294a253c68"
+}
+
+variable "openstack_user_domain_name" {
+    description = "OpenStack domain name"
+    type = string
+    default = "Default"
+}
+
+variable "openstack_region_name" {
+    description = "OpenStack region name"
+    type = string
+    default = "gibson"
+}
+
+variable "openstack_project_domain_id" {
+    description = "OpenStack project domain ID"
+    type = string
+    default = "default"
+}
