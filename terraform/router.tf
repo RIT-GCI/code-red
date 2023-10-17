223909146f8b29c652c479bd0d4f776ae056f3de
Ashim Mahara <katyansun@gmail.com>
2023-10-17 16:32:54 -0400

    created different networks, created test instances for each of the networks

--- terraform/router.tf
+++ terraform/router.tf
@@ -4,15 +4,28 @@ resource "openstack_networking_router_v2" "router" {
 
 }
 
-# resource "openstack_networking_network_v2" "externalNetwork" {
-#   name           = "externalNetwork"
-#   admin_state_up = "true"
-# }
-
-resource "openstack_networking_network_v2" "internalNetwork"{
-  name = "internalNetwork"
+resource "openstack_networking_network_v2" "business_network"{
+  name = "business_network"
   admin_state_up = "true"
 }
+
+resource "openstack_networking_network_v2" "enterprise_network"{
+  name = "enterprise_network"
+  admin_state_up = "true"
+}
+
+
+resource "openstack_networking_network_v2" "control_room_network"{
+  name = "control_room_network"
+  admin_state_up = "true"
+}
+
+
+resource "openstack_networking_network_v2" "soc_network"{
+  name = "soc_network"
+  admin_state_up = "true"
+}
+
 
 # subnet pool for the whole of internal network
 # consists of the Business LAN, Enterprise LAN, Control System LAN, SOC/Monitoring LAN
@@ -21,23 +34,10 @@ resource "openstack_networking_subnetpool_v2" "internalNetworkPool" {
   prefixes = ["10.10.0.0/16"]
 }
 
-# external network may consist of AD/DC
-# resource "openstack_networking_subnet_v2" "externalNetwork_subnet_1" {
-#   network_id = openstack_networking_network_v2.externalNetwork.id
-#   cidr       = "192.168.101.0/24"
-#   ip_version = 4
-# }
-
-# resource "openstack_networking_router_interface_v2" "router_external_interface" {
-#   router_id = openstack_networking_router_v2.router.id
-#   subnet_id = openstack_networking_subnet_v2.externalNetwork_subnet_1.id
-# }
-
-
 resource "openstack_networking_subnet_v2" "business_lan" {
   name          = "business_lan"
   cidr          = "10.10.10.0/24"
-  network_id    = openstack_networking_network_v2.internalNetwork.id
+  network_id    = openstack_networking_network_v2.business_network.id
   subnetpool_id = openstack_networking_subnetpool_v2.internalNetworkPool.id
 }
 
@@ -49,7 +49,7 @@ resource "openstack_networking_router_interface_v2" "router_business_lan_interfa
 resource "openstack_networking_subnet_v2" "enterprise_lan" {
   name          = "enterprise_lan"
   cidr          = "10.10.20.0/24"
-  network_id    = openstack_networking_network_v2.internalNetwork.id
+  network_id    = openstack_networking_network_v2.enterprise_network.id
   subnetpool_id = openstack_networking_subnetpool_v2.internalNetworkPool.id
 }
 
@@ -61,7 +61,7 @@ resource "openstack_networking_router_interface_v2" "router_enterprise_lan_inter
 resource "openstack_networking_subnet_v2" "control_room_lan" {
   name          = "control_room_lan"
   cidr          = "10.10.30.0/24"
-  network_id    = openstack_networking_network_v2.internalNetwork.id
+  network_id    = openstack_networking_network_v2.control_room_network.id
   subnetpool_id = openstack_networking_subnetpool_v2.internalNetworkPool.id
 }
 
@@ -73,7 +73,7 @@ resource "openstack_networking_router_interface_v2" "router_control_room_lan_int
 resource "openstack_networking_subnet_v2" "soc_lan" {
   name          = "soc_lan"
   cidr          = "10.10.40.0/24"
-  network_id    = openstack_networking_network_v2.internalNetwork.id
+  network_id    = openstack_networking_network_v2.soc_network.id
   subnetpool_id = openstack_networking_subnetpool_v2.internalNetworkPool.id
 }
 
