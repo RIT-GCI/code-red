c899f610111ce7de1680465152d452df40eb7508
gido5731 <gido@gido.click>
2023-10-30 15:22:36 -0400

    First attempt at making data nodes for elastic

--- terraform/socInstances.tf
+++ terraform/socInstances.tf
@@ -14,7 +14,8 @@ resource "openstack_compute_instance_v2" "ansible" {
                    }]
     "hostsfile" = file("bootstrap/ansiblehosts")
     ips = {
-      "elasticsearch" = openstack_networking_port_v2.elasticsearch_port1.fixed_ip[0].ip_address
+      "elasticsearch" = openstack_networking_port_v2.elasticsearch_port1.fixed_ip[0].ip_address,
+      "elasticsearchdata1" = openstack_networking_port_v2.elasticsearchdata1_port1.fixed_ip[0].ip_address
     }
     usersshkeys = [for f in fileset(path.module, "ssh_keys/id_*.pub"): {
                         filename = f
@@ -47,8 +48,7 @@ resource "openstack_networking_floatingip_associate_v2" "fip1_association" {
 }
 
 
-
-
+#elastic-land
 resource "openstack_compute_instance_v2" "elasticsearch" {
   name              = "elasticsearch"
   image_name        = "DebianBullseye11"
@@ -73,5 +73,32 @@ resource "openstack_networking_port_v2" "elasticsearch_port1" {
   fixed_ip {
     subnet_id  = openstack_networking_subnet_v2.soc_lan.id
     ip_address = "10.10.40.201"
+  }
+}
+
+resource "openstack_compute_instance_v2" "elasticsearchdata1" {
+  name              = "elasticsearchdata1"
+  image_name        = "DebianBullseye11"
+  flavor_name       = "small"
+  key_pair          = "bootstrap"
+  security_groups   = ["default"]
+
+  user_data = templatefile("bootstrap/bootstrapelastic.sh.tftpl", {
+    "bootstrapsshpubkey": file("bootstrap/id_bootstrap.pub")
+  })
+
+  network {
+    port = openstack_networking_port_v2.elasticsearchdata1_port1.id
+  }
+}
+
+resource "openstack_networking_port_v2" "elasticsearchdata1_port1" {
+  name               = "elasticsearchdata1_port1"
+  network_id         = openstack_networking_network_v2.soc_network.id
+  admin_state_up     = "true"
+
+  fixed_ip {
+    subnet_id  = openstack_networking_subnet_v2.soc_lan.id
+    ip_address = "10.10.40.202"
   }
 }
