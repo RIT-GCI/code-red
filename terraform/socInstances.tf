edcdf7086348ce708e93c052b5d9a02809865692
Ashim Mahara <katyansun@gmail.com>
2023-11-02 19:34:14 -0400

    Reogranized the Ansible Directory Structure
    - Moved everything related to ansible to ansible
    - Refactored code to reflect the changes
    - Changed generateusers.yml content to standard YAML key-value representation -> key: value
    - Fixed the password escape character bug for ashim user

--- terraform/socInstances.tf
+++ terraform/socInstances.tf
@@ -6,18 +6,18 @@ resource "openstack_compute_instance_v2" "ansible" {
   security_groups   = ["default"]
 
 
-  user_data = templatefile("bootstrap/bootstrapansible.sh.tftpl", {
-    bootstrapsshprivkey = file("bootstrap/id_bootstrap")
-    playbooks = [for f in fileset(path.module, "playbooks/*.yml"): {
+  user_data = templatefile("ansible/bootstrap/bootstrapansible.sh.tftpl", {
+    bootstrapsshprivkey = file("ansible/bootstrap/id_bootstrap")
+    playbooks = [for f in fileset(path.module, "ansible/playbooks/*.yml"): {
                         filename = f
                         content = file(f)
                    }]
-    "hostsfile" = file("bootstrap/ansiblehosts")
+    "hostsfile" = file("ansible/bootstrap/ansiblehosts")
     ips = {
       "elasticsearch" = openstack_networking_port_v2.elasticsearch_port1.fixed_ip[0].ip_address,
       "elasticsearchdata1" = openstack_networking_port_v2.elasticsearchdata1_port1.fixed_ip[0].ip_address
     }
-    usersshkeys = [for f in fileset(path.module, "ssh_keys/id_*.pub"): {
+    usersshkeys = [for f in fileset(path.module, "ansible/ssh_keys/id_*.pub"): {
                         filename = f
                         content = file(f)
                    }]
@@ -56,8 +56,8 @@ resource "openstack_compute_instance_v2" "elasticsearch" {
   key_pair          = "gframe"
   security_groups   = ["default"]
 
-  user_data = templatefile("bootstrap/bootstrapelastic.sh.tftpl", {
-    "bootstrapsshpubkey": file("bootstrap/id_bootstrap.pub")
+  user_data = templatefile("ansible/bootstrap/bootstrapelastic.sh.tftpl", {
+    "bootstrapsshpubkey": file("ansible/bootstrap/id_bootstrap.pub")
   })
 
   network {
@@ -83,8 +83,8 @@ resource "openstack_compute_instance_v2" "elasticsearchdata1" {
   key_pair          = "gframe"
   security_groups   = ["default"]
 
-  user_data = templatefile("bootstrap/bootstrapelastic.sh.tftpl", {
-    "bootstrapsshpubkey": file("bootstrap/id_bootstrap.pub")
+  user_data = templatefile("ansible/bootstrap/bootstrapelastic.sh.tftpl", {
+    "bootstrapsshpubkey": file("ansible/bootstrap/id_bootstrap.pub")
   })
 
   network {
